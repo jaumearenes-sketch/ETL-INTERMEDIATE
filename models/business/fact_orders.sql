33742553ff4a3f6eeb671dd748bfbccd5fe10daa
Jaume <jaume.arenes@sdggroup.com>
2026-06-01 08:43:06 +0000

    Dimensiones relacionadas con el fact en lineage

--- models/business/fact_orders.sql
+++ models/business/fact_orders.sql
@@ -10,15 +10,39 @@ orders_enriched as (
 
 ),
 
+customer as (
+
+    select customer_key from {{ ref('dim_customer') }}
+
+),
+
+part as (
+
+    select part_key from {{ ref('dim_part') }}
+
+),
+
+supplier as (
+
+    select supplier_key from {{ ref('dim_supplier') }}
+
+),
+
+date_dim as (
+
+    select date_key from {{ ref('dim_date') }}
+
+),
+
 final as (
 
     select
         lineitem_enriched.lineitem_key as fact_key,
         lineitem_enriched.order_key,
-        lineitem_enriched.part_key,
-        lineitem_enriched.supplier_key,
-        orders_enriched.customer_key,
-        orders_enriched.order_date as date_key,
+        part.part_key,
+        supplier.supplier_key,
+        customer.customer_key,
+        date_dim.date_key,
         lineitem_enriched.ship_date,
         lineitem_enriched.quantity,
         lineitem_enriched.extended_price,
@@ -39,8 +63,11 @@ final as (
         orders_enriched.priority_category
     from lineitem_enriched
     inner join orders_enriched using (order_key)
+    inner join customer on orders_enriched.customer_key = customer.customer_key
+    inner join part on lineitem_enriched.part_key = part.part_key
+    inner join supplier on lineitem_enriched.supplier_key = supplier.supplier_key
+    inner join date_dim on orders_enriched.order_date = date_dim.date_key
 
 )
 
 select * from final
-
