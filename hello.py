4190c9f2900c31b1593bbf7cf351cc5ac512767a
Jaume <jaume.arenes@sdggroup.com>
2026-06-04 12:17:13 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,21 @@
+from prefect import flow, task
+
+
+@task
+def create_message():
+    msg = 'Hello from a task!'
+    return msg
+
+@flow
+def something_else():
+    result = 10
+    return (result)
+
+@flow
+def hello_world():
+    sub_flow_message = something_else()
+    task_message = create_message()
+    new_message = task_message + str(sub_flow_message)
+    print(new_message)
+
+hello_world()
